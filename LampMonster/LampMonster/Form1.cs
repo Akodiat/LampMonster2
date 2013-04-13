0d0680f04793d1edee978f153913cf862869518d
Lukas Kurtyan <kurtyan@student.chalmers.se>
2013-04-13 13:44:59 +0200

    Implemented an indomain model implementation of NaiveBayes (copied most from C++ version.
    ATM it is EXTREMLY SLOW since i am using a third pary library for
    arbitrary precicion floaing points.

--- LampMonster/LampMonster/Form1.cs
+++ LampMonster/LampMonster/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +17,74 @@ namespace LampMonster
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string root = "C:/Users/Lukas/Documents/GitHub/LampMonster/Documents/amazon-balanced-6cats";
+
+            var categories = new List<string>();
+            categories.Add("books");
+            categories.Add("camera");
+            categories.Add("DVD");
+            categories.Add("music");
+            categories.Add("health");
+            categories.Add("software");
+
+            var pathFinder = new OrderedPathFinder(500, 400);
+            var fileParser = new FileParser();
+
+            foreach (var item in categories)
+            {
+                var posPath = root + "/" + item + "/pos";
+                var negPath = root + "/" + item + "/neg";
+
+                var pos = pathFinder.GetTrainingPaths(posPath);
+                var neg = pathFinder.GetTrainingPaths(negPath);
+
+                var map = new Dictionary<string, List<string>>();
+                map.Add("pos", GetMegaDocument(pos, fileParser));
+                map.Add("neg", GetMegaDocument(neg, fileParser));
+
+                var classifyer = new NaiveBayesClassifyer(map, 1);
+
+                var pos2 = pathFinder.GetProcessingPaths(posPath);
+                var neg2 = pathFinder.GetProcessingPaths(negPath);
+
+                int correctPosCount = 0;
+
+                foreach (var document in pos2)
+                {
+                    var category = classifyer.Classify(fileParser.GetWordsInFile(document, (s) => true));
+                    if (category == "pos")
+                    {
+                        correctPosCount++;
+                    }
+                }
+                
+                Console.WriteLine("Correct pos for category " + item + " is: " + correctPosCount);
+                int correctNegCount = 0;
+
+                foreach (var document in neg2)
+                {
+                    var category = classifyer.Classify(fileParser.GetWordsInFile(document, (s) => true));
+                    if (category == "neg")
+                        correctNegCount++;
+                }
+
+                Console.WriteLine("Correct pos neg category " + item + " is: " + correctNegCount);
+                Console.WriteLine("Total correct for " + item + " is: " + (correctPosCount + correctNegCount));           
+            }
+       }
+
+        private List<string> GetMegaDocument(List<string> files, FileParser parser)
+        {
+            double dummy;
+            var words = new List<string>();
+            foreach (var file in files)
+                words.AddRange(parser.GetWordsInFile(file, (s) => !double.TryParse(s, out dummy)));
+
+            return words;
+        }
+        
     }
 }
---
+++ LampMonster/LampMonster/NaiveBayesClassifyer.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using W3b.Sine;
+
+
+namespace LampMonster
+{
+    class NaiveBayesClassifyer : Classifyer
+    {
+        private class Category
+        {
+            public readonly string id;
+            public readonly int vocabalarySize;
+            public readonly Dictionary<string, double> bagOfWords;
+
+            public Category(string id, int vocabalarySize, Dictionary<string, double> bagOfWords)
+            {
+                this.id = id;
+                this.vocabalarySize = vocabalarySize;
+                this.bagOfWords = bagOfWords;
+            }
+        }
+
+
+        List<Category> categories;
+        double prior;
+
+        public NaiveBayesClassifyer(Dictionary<string, List<string>> trainingSet,
+                                     double prior)
+        {
+            this.categories = CreateCategorys(trainingSet);
+            this.prior = prior;
+        }
+
+
+        private static List<Category> CreateCategorys(Dictionary<string, List<string>> trainingSet)
+        {
+            var categories = new List<Category>();
+            foreach (var item in trainingSet)
+            {
+                var bag = CreateMegaDoc(item.Value); 
+                categories.Add(new Category(item.Key, item.Value.Count, bag));
+            }
+
+            return categories;
+        }
+
+        private static Dictionary<string, double> CreateMegaDoc(List<string> words)
+        {
+            var bag = new Dictionary<string, double>();
+            foreach (var word in words)
+            {
+                if (!bag.ContainsKey(word))
+                    bag[word] = 1.0d;
+                else
+                    bag[word]++;
+            }
+
+
+
+            return bag;
+        }
+
+
+        private BigNum CalculateCategoryProbability(List<string> document, Category category) 
+        {
+            BigNum product = 1;
+            BigNum nom = new BigNumDec(category.vocabalarySize + category.bagOfWords.Count * this.prior);
+
+            foreach (var word in document)
+            {
+                double denom;
+                category.bagOfWords.TryGetValue(word, out denom);
+                denom += prior;
+                product = new BigNumDec(denom) / nom;
+            }
+
+            return product;
+        }
+
+        public string Classify(List<string> document)
+        {
+            BigNum max = 0.0d;
+            Category selectedCategory = null;
+            foreach (var category in this.categories)
+            {
+                BigNum probability = CalculateCategoryProbability(document, category);
+                if (probability.CompareTo(max) < 0)
+                {
+                    max = probability;
+                    selectedCategory = category;
+                }
+            }
+
+            return selectedCategory.id;
+        }
+    }
+}
