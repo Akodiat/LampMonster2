796fcb0b38c212ed37ab3415b02cd6053e08d8b1
Akodiat <joakimjo@swipnet.se>
2013-04-17 18:19:49 +0200

    McNemar start

---
+++ LampMonster/LampMonster/McNemar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LampMonster
+{
+    class McNemar
+    {
+        double calcTestStatistic(int pos_neg, int neg_pos)
+    }
+}
